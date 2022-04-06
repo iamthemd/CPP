e7e7693732ba20175f652afaba6da3d8b499600a
MayankPatel <mayankpatel.ahm@gmail.com>
2022-04-06 23:57:42 +0530

    Dwestructor added

--- MyCpp/Inheritance/Inheritance.cpp
+++ MyCpp/Inheritance/Inheritance.cpp
@@ -6,16 +6,31 @@ Base::Base() {
 	std::cout << "Base Class Constructor\n";
 }
 
+Base::~Base() {
+	std::cout << "Base Class Destructor\n";
+}
+
 Derive::Derive() {
 	std::cout << "Derive Class Constructor\n";
+}
+
+Derive::~Derive() {
+	std::cout << "Derive Class Destructor\n";
 }
 
 Derive1::Derive1() {
 	std::cout << "Derive1 Class Constructor\n";
 }
 
+Derive1::~Derive1() {
+	std::cout << "Derive1 Class Destructor\n";
+}
+
 void Test_Inheritnce() {
+	// Base Class Constructor first called then Derive class constructor called.
+	// In Destructor case first derive class destructor destruct then base class destructor
 	Base b;
 	Derive d;
 	Derive1 d1;
+
 }
--- MyCpp/Inheritance/Inheritance.h
+++ MyCpp/Inheritance/Inheritance.h
@@ -4,16 +4,19 @@ namespace Inheritance{
 class Base{
 public:
 	Base();
+	~Base();
 };
 
 class Derive : public Base {
 public:
 	Derive();
+	~Derive();
 };
 
 class Derive1 : public Derive {
 public:
 	Derive1();
+	~Derive1();
 };
 
 }
