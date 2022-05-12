0bcfa9344116a6b912a797a0280fe4db7c98d7fd
MayankPatel <mayankpatel.ahm@gmail.com>
2022-05-12 15:24:34 +0530

    Virtual Concept

--- MyCpp/Polymorphism/VirtualConcept.h
+++ MyCpp/Polymorphism/VirtualConcept.h
@@ -1,13 +1,15 @@
 #pragma once
 
 /* Virtual Function: Virtual function declare with virtual keyword.
-*  when you declare function as virtual, it tells compiler to take decision at runtime or perform dynamic binding on the function.
+*  when you declare function as virtual, it tells compiler to take decision at runtime
+*  or perform dynamic binding on the function.
 *  Why?
-*   : When it is necessary to have single pointer to refer all object of different class, this can only be possibe by creating base
-*     class's pointer and refer to all derived class object.
-*     But problem is when we use base class's pointer it always refer to base class's object and called base class method, to resolve
-*     this problem we use function as virutal function which tells compiler to take decision at runtime and check in vtable where 
-*     basse class pointer pointing.
+*   : When it is necessary to have single pointer to refer all object of different class,
+*     this can only be possibe by creating base class's pointer and refer to all derived class object.
+*   - But problem is when we use base class's pointer it always refer to base class's object
+*     and called base class method, to resolve
+*   - this problem we use function as virutal function which tells compiler to take decision at 
+*     runtime and check in vtable where base class pointer pointing.
 */
 class BaseClass {
 public:
