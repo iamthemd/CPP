7047876b4b690fd9a62cd4d7f1aae7f122a4a7f6
MayankPatel <mayankpatel.ahm@gmail.com>
2022-06-06 17:48:52 +0530

    PreDefine Smart PTr

---
+++ MyCpp/SmartPointer/PreDefinSmartPtr.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+
+void Test_UniquePtr() {
+	std::cout << "Unique Ptr" << std::endl;
+}
+
+void Test_SharedPtr() {
+
+}
+
+void Test_WeakPtr() {
+
+}
+
+void Test_PreDefineSmartpointer() {
+	Test_UniquePtr();
+	Test_SharedPtr();
+	Test_WeakPtr();
+}
